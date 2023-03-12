e45ec064974c8868790f257e9674edb149e20c75
Danjka99 <jakob_daniel@gmx.net>
2023-03-12 19:35:28 +0100

    sync

--- DateTime/DateTime/Form1.Designer.cs
+++ DateTime/DateTime/Form1.Designer.cs
@@ -33,6 +33,12 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -43,6 +49,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Zeitmessung";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
@@ -51,7 +58,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(258, 20);
             this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Visible = false;
             // 
             // label1
             // 
@@ -61,6 +68,7 @@
             this.label1.Size = new System.Drawing.Size(49, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Start-Nr.:";
+            this.label1.Visible = false;
             // 
             // textBox2
             // 
@@ -70,21 +78,86 @@
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.textBox2.Size = new System.Drawing.Size(274, 200);
             this.textBox2.TabIndex = 4;
+            this.textBox2.Visible = false;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(726, 9);
+            this.label2.Location = new System.Drawing.Point(510, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(61, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Messwerte:";
+            this.label2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 356);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(292, 82);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Startnummer ausbessern";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(15, 330);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Visible = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(198, 330);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(106, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 314);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Zeile:";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(195, 314);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Neue Startnummer:";
+            this.label4.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(514, 365);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(274, 73);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Start der Stoppuhr";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
@@ -104,6 +177,12 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- DateTime/DateTime/Form1.cs
+++ DateTime/DateTime/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,9 +15,15 @@ namespace DateTime
     {
         int zaehler = 0;
 
+        int korrektur;
+
+        int zeilennummer;
+
         string[] zeit = new string[1000];
 
-        string [] nummer = new string[1000];
+        List<string> nummer1 = new List<string>();
+
+        Stopwatch stoppuhr = new Stopwatch();
 
         public Form1()
         {
@@ -26,18 +33,51 @@ namespace DateTime
         private void button1_Click(object sender, EventArgs e)
         {
             string newLine = Environment.NewLine;
-            if (!string.IsNullOrEmpty(nummer[zaehler]))
+            if (!textBox1.Text.Equals("") && !nummer1.Contains(textBox1.Text))
             {
-                zeit[zaehler] = System.DateTime.Now.ToString("hh:mm:ss.ff");
-                textBox2.Text += newLine + "Startnummer: " + nummer[zaehler] + " | " + zeit[zaehler];
+                TimeSpan ts = stoppuhr.Elapsed;
+                string zeitablauf = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+
+                nummer1.Add(textBox1.Text);
+                zeit[zaehler] = zeitablauf;
+                textBox2.Text += newLine + (zaehler + 1).ToString() + " Startnummer: " + nummer1[zaehler] + " | " + zeit[zaehler];
                 zaehler++;
             }
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-            nummer[zaehler] = textBox1.Text;
+            string[] zeile = textBox2.Lines;
+            if (!string.IsNullOrEmpty(textBox3.Text) && !string.IsNullOrEmpty(textBox4.Text))
+            {
+                korrektur = int.Parse(textBox4.Text);
+                zeilennummer = (int.Parse(textBox3.Text) - 1);
+                if (!nummer1.Contains(korrektur.ToString()) && !(zeilennummer > nummer1.Count))
+                {
+                    nummer1.RemoveAt(zeilennummer);
+                    nummer1.Insert(zeilennummer, korrektur.ToString());
+                    zeile[zeilennummer + 1] = (zeilennummer + 1).ToString() + " Startnummer: " + nummer1[zeilennummer] + " | " + zeit[zeilennummer];
+                    textBox2.Lines = zeile;
+                }
+            }
         }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            stoppuhr.Start();
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+            textBox4.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = false;
+        }
     }
 }
